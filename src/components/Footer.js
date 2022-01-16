7aeaa8226f935539754b42feb6a8eab4f6ac3f47
ltherisod <lauratherisod@gmail.com>
2022-01-15 21:37:22 -0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            <h5>Laura Therisod || 2021</h5>
+            <h5>Laura Therisod || 2022 || Argentina</h5>
         </footer>
     )
 }
